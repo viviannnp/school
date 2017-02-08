6e6e127ab6fe74159c6f79e0fd3209d39fe1e580
Daryl Nakamoto <djnakamoto@gmail.com>
2017-02-08 00:45:29 -0800

    Hw4 Complete?
    Complete hw4 (maybe)

--- UCD/hw4/testmystringfunctions.c
+++ UCD/hw4/testmystringfunctions.c
@@ -25,21 +25,21 @@ void flushStdin();
 // For grading purposes, we ask the user to pick the test.
 // DO NOT modify this function for your homework.
 int main() {
-  // User menu
-  printf("Which unit test would you like to run?\n");
-  printf("1) deepCopyStr\n");
-  printf("\ta) n = 2, src = \"test string\"\n");
-  printf("\tb) n = 0 returns \"\\0\"\n");
-  printf("\tc) negative n returns NULL\n");
-  printf("2) isLowerOrDigitStr\n");
-  printf("\ta) n = 4, src = \"testString\"\n");
-  printf("\tb) n = 5, src = \"testString\"\n");
-  printf("\tc) n = 0\n");
-  printf("3) concatStrs\n");
-  printf("\ta) n = 5, str1 = \"test\", str2 = \"string\"\n");
-  printf("\tb) n = 5, str1 = \"\", str2 = \"test string\" returns \"test \"\n");
-  printf("\tc) n = 5, str1 = \"test\", str2 = \"\" returns NULL\n");
-  while (!getAndRunTest()) {}
+   // User menu
+   printf("Which unit test would you like to run?\n");
+   printf("1) deepCopyStr\n");
+   printf("\ta) n = 2, src = \"test string\"\n");
+   printf("\tb) n = 0 returns \"\\0\"\n");
+   printf("\tc) negative n returns NULL\n");
+   printf("2) isLowerOrDigitStr\n");
+   printf("\ta) n = 4, src = \"testString\"\n");
+   printf("\tb) n = 5, src = \"testString\"\n");
+   printf("\tc) n = 0\n");
+   printf("3) concatStrs\n");
+   printf("\ta) n = 5, str1 = \"test\", str2 = \"string\"\n");
+   printf("\tb) n = 5, str1 = \"\", str2 = \"test string\" returns \"test \"\n");
+   printf("\tc) n = 5, str1 = \"test\", str2 = \"\" returns NULL\n");
+   while (!getAndRunTest()) {}
 }
 
 // Prompt user once for the test to run. 1a is deepCopyStrTestA,
@@ -48,98 +48,159 @@ int main() {
 // If the user input is invalid, print the error according to the homework
 // prompt and return false.
 bool getAndRunTest() {
-  // Homework TODO: complete the code for this function.
-  char userInput[3] = {'\0', '\0', '\0'};
+   // Homework TODO(COMPLETE): complete the code for this function.
+   char userInput[3] = {'\0', '\0', '\0'};
 
-  fgets(userInput, 3, stdin);
-  flushStdin();
-  printf("%c\n", userInput[0]);
+   fgets(userInput, 3, stdin);
+   flushStdin();
 
-  if (userInput[0] == 1 || userInput[0] == 2 || userInput[0] == 3) {
-    switch (userInput[1]) {
-      case 'a':
-        return true;
+   if (userInput[0] == '1') {
+      switch (userInput[1]) {
+         case 'a':
+         deepCopyStrTestA();
+         return true;
 
-      case 'b':
-        return true;
+         case 'b':
+         deepCopyStrTestB();
+         return true;
 
-      case 'c':
-        return true;
+         case 'c':
+         deepCopyStrTestC();
+         return true;
 
-      default:
-        printf("Enter a, b, or c for the test case.\n");
-        return false;
-     }
-  }
-  else {
-    printf("Enter 1, 2, or 3 for the function to test.\n");
-    return false;
-  }
+         default:
+         printf("Enter a, b, or c for the test case.\n");
+         return false;
+      }
+   }
+   else if (userInput[0] == '2') {
+      switch (userInput[1]) {
+         case 'a':
+         isLowerOrDigitStrTestA();
+         return true;
+
+         case 'b':
+         isLowerOrDigitStrTestB();
+         return true;
+
+         case 'c':
+         isLowerOrDigitStrTestC();
+         return true;
+
+         default:
+         printf("Enter a, b, or c for the test case.\n");
+         return false;
+      }
+   }
+   else if (userInput[0] == '3') {
+      switch (userInput[1]) {
+         case 'a':
+         concatStrsTestA();
+         return true;
+
+         case 'b':
+         concatStrsTestB();
+         return true;
+
+         case 'c':
+         concatStrsTestC();
+         return true;
+
+         default:
+         printf("Enter a, b, or c for the test case.\n");
+         return false;
+      }
+   }
+   else {
+      printf("Enter 1, 2, or 3 for the function to test.\n");
+      return false;
+   }
 }
 
 // Test n = 2 and src = "test string" returns "te"
 void deepCopyStrTestA() {
-      char* result = deepCopyStr("test string", 2);
-      // if (result) checks to see something is returned (even if the string is empty).
-      //   We will see later in the course this is checking if the result is a NULL pointer
-      assert(result && result[0] == 't' && result[1] == 'e' && result[2] == '\0');
-      printf("Test successful.\n");
+   char* result = deepCopyStr("test string", 2);
+   // if (result) checks to see something is returned (even if the string is empty).
+   //   We will see later in the course this is checking if the result is a NULL pointer
+   assert(result && result[0] == 't' && result[1] == 'e' && result[2] == '\0');
+   printf("Test successful.\n");
 }
 
 // Test n = 0 the returns "\0"
 void deepCopyStrTestB() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   char* result = deepCopyStr("test string", 0);
+
+   assert(result == '\0');
+   printf("Test successful.\n");
 }
 
 // Test negative n returns NULL"
 void deepCopyStrTestC() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   char* result = deepCopyStr("test string", -1);
+
+   assert(result == NULL);
+   printf("Test successful.\n");
 }
 
 // Test n = 4, src = "testString" returns true
 void isLowerOrDigitStrTestA() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   assert(isLowerOrDigitStr("testString", 4));
+   printf("Test successful.\n");
 }
 
 // Test n = 5, src = "testString" returns false
 void isLowerOrDigitStrTestB() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   assert(isLowerOrDigitStr("testString", 5));
+   printf("Test successful.\n");
 }
 
 // Test n = 0, src = "" returns true
 void isLowerOrDigitStrTestC() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   assert(isLowerOrDigitStr("", 0));
+   printf("Test successful.\n");
 }
 
 // Test n = 10, str1 = "test", str2 = "string" returns "teststring"
 void concatStrsTestA() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   assert(strcmp(concatStrs("test", "string", 10), "teststring") == 0);
+   printf("Test successful.\n");
 }
 
 // n = 5, str1 = "", str2 = "test string" returns "test "
 void concatStrsTestB() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   //assert(strcmp(concatStrs("", "test string", 5), "test") == 0);
+   assert(strcmp(concatStrs("", "test string", 5), "test") == 0);
+   printf("Test successful.\n");
 }
 
 // n = 5, str1 = "test", str2 = "" returns NULL
 void concatStrsTestC() {
-    // Homework TODO: write code for this test case according to the
-    // specifications in the comment above the function.
+   // Homework TODO: write code for this test case according to the
+   // specifications in the comment above the function.
+   assert(concatStrs("test", "", 5) == NULL);
+   printf("Test successful.\n");
 }
 
 // Flush stdin buffer
 void flushStdin() {
-  char c;
-  // Skip all characters until end-of-file marker
-  // or new line / carriage return
-  while ( (c = getchar()) != EOF &&
-          c != '\n' &&
-          c != '\r' ) {};
+   char c;
+   // Skip all characters until end-of-file marker
+   // or new line / carriage return
+   while ( (c = getchar()) != EOF &&
+   c != '\n' &&
+   c != '\r' ) {};
 }
